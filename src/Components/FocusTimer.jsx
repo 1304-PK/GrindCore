1f2010bdad0a79368ec1870fcee4205a34e0b06e
1304-PK <pushkark1304@gmail.com>
2025-07-19 07:08:48 +0530

    toggle between run and pause button

--- src/Components/FocusTimer.jsx
+++ src/Components/FocusTimer.jsx
@@ -1,62 +1,3 @@
-
-// import { useState, useEffect } from "react"
-// import '../Styles/FocusTimer.css'
-
-// const FocusTimer = () => {
-//     const [second, setSecond] = useState(0)
-//     const [minute, setMinute] = useState(0)
-//     const [run, toggleRun] = useState(false)
-//     const [focusTime, setFocusTime] = useState()
-//     const [breakTime, setBreakTime] = useState()
-
-//     useEffect(() => {
-//         if (second === 60) {
-//             setSecond(0)
-//             setMinute(prev => prev + 1)
-//         }
-//     }, [second])
-
-//     useEffect(() => {
-//         if (!run) { return }
-
-//         const timer = setInterval(() => {
-//             setSecond(prev => prev + 1)
-//         }, 1000);
-
-//         return () => {
-//             clearInterval(timer)
-//         }
-//     }, [run])
-
-
-//     return (
-//         <div id="focus-timer">
-//             <h1 id="heading">Focus Timer</h1>
-//             <div id="timer">
-//                 {minute}:{second}
-//             </div>
-//             <div id="timer-buttons-div">
-//                 <button onClick={() => { toggleRun(true) }}>Start</button>
-//                 <button onClick={() => { toggleRun(false) }}>Pause</button>
-//                 <button onClick={() => { toggleRun(false); setSecond(0); setMinute(0) }}>Reset</button>
-//                 <button onClick={() => {document.getElementById('settings-dialog').showModal()}}>Settings</button>
-//             </div>
-//             <dialog id="settings-dialog">
-//                 <h1>Settings</h1>
-//                 <form>
-//                     <label htmlFor="focus-time">Focus Time</label>
-//                     <input type="number" id="focus-time" value={focusTime} onChange={(e)=> {setFocusTime(e.target.value)}} />
-//                     <label htmlFor="break-time">Break Time</label>
-//                     <input type="number" id="break-time" value={breakTime} onChange={(e)=> {setBreakTime(e.target.value)}}/>
-//                     <button onClick={(e) => {e.preventDefault();document.getElementById('settings-dialog').close()}}>Submit</button>
-//                 </form>
-//             </dialog>
-//         </div>
-//     )
-// }
-
-// export default FocusTimer
-
 import { useState, useEffect } from 'react'
 import '../Styles/FocusTimer.css'
 
@@ -65,13 +6,17 @@ const FocusTimer = () => {
     const [minute, setMinute] = useState(0)
     const [button, toggleButton] = useState('Start')
     const [run, toggleRun] = useState(false)
+    const [breakTime, setBreakTime] = useState(1)
+    const [focusTime, setFocusTime] = useState(1)
+    const [breakTimeValue, setBreakTimeValue] = useState(1)
+    const [focusTimeValue, setFocusTimeValue] = useState(1)
 
     const updateTimer = () => {
-        if (button === "Start"){
+        if (button === "Start") {
             toggleButton("Pause")
             toggleRun(true)
         }
-        else{
+        else {
             toggleButton("Start")
             toggleRun(false)
         }
@@ -79,10 +24,10 @@ const FocusTimer = () => {
 
     useEffect(() => {
         const timer = setInterval(() => {
-            setSecond(prev => prev+1)
+            setSecond(prev => prev + 1)
         }, 1000);
 
-        if (!run){
+        if (!run) {
             clearInterval(timer)
         }
 
@@ -91,28 +36,55 @@ const FocusTimer = () => {
         }
     }, [run])
 
+    // Update minute on passing 60 seconds
     useEffect(() => {
-        if (second === 5){
-            setMinute(prev => prev+1)
+        if (second === 5) {
+            setMinute(prev => prev + 1)
             setSecond(0)
-
-            // for changing the number without looking like a glitch, setinterval for 950ms to change the minute 
-            // and then clear the interval
         }
     }, [second])
 
-    return(
-        <div id="focus-timer">
-            <h1 id='heading'>Focus Timer</h1>
-            <div id='timer'>
-                {minute}:{second}
+    // Handle completion of Timer
+    useEffect(() => {
+        if (minute === Number(focusTimeValue)){
+            alert('Timer Completed')
+            toggleRun(false)
+        }
+    }, [minute])
+
+    return (
+        <>
+            <div id="main-container">
+                <h1 id='heading'>Focus Timer</h1>
+                <div id='timer'>
+                    {minute}:{second}
+                </div>
+                <div id='timer-buttons-div'>
+                    <button onClick={updateTimer}>{button}</button>
+                    <button onClick={() => { toggleRun(false); setSecond(0); setMinute(0); toggleButton("Start") }}>Reset</button>
+                    <button onClick={() => { document.getElementById('timer-settings-dialog').showModal() }}>Settings</button>
+                </div>
             </div>
-            <div id='timer-buttons-div'>
-                <button onClick={updateTimer}>{button}</button>
-                <button onClick={() => {toggleRun(false);setSecond(0);setMinute(0);toggleButton("Start")}}>Reset</button>
-                <button>Settings</button>
-            </div>
-        </div>
+
+            <dialog id="timer-settings-dialog">
+                <h1>Settings</h1>
+                <form>
+                    <div>
+                        <label htmlFor="focus-timer-input">Focus Timer</label>
+                        <input type="number" id="focus-timer-input" value={focusTimeValue} onChange={(e) => {setFocusTimeValue(e.target.value)}} required/>
+                    </div>
+                    <div>
+                        <label htmlFor="break-time">Break</label>
+                        <input type="number" id="break-timer"  value={breakTimeValue} onChange={(e) => {setBreakTimeValue(e.target.value)}} required/>
+                    </div>
+
+                    <div id='form-buttons'>
+                        <button id="cancel-form" onClick={(e) => {e.preventDefault();document.getElementById('timer-settings-dialog').close()}}>Cancel</button>
+                        <button id="submit-form" onClick={(e) => {e.preventDefault();document.getElementById('timer-settings-dialog').close()}}>Submit</button>
+                    </div>
+                </form>
+            </dialog>
+        </>
     )
 }
 
